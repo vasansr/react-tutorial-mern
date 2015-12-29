0b0e8cf11247bd914c2657b9bf7f2198b0b85500
Vasan Subramanian <vasan.srini@gmail.com>
2015-12-29 17:47:12 +0530

    Step 3.3: Compose Components - communicate

--- src/App.js
+++ src/App.js
@@ -6,10 +6,38 @@ var BugFilter = React.createClass({
   }
 });
 
+var BugRow = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.id}</td>
+        <td>{this.props.status}</td>
+        <td>{this.props.priority}</td>
+        <td>{this.props.owner}</td>
+        <td>{this.props.title}</td>
+      </tr>
+    )
+  }
+});
+
 var BugTable = React.createClass({
   render: function() {
     return (
-      <div>The list of bugs as a table would come here.</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Priority</th>
+            <th>Owner</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <BugRow id={1} priority="P1" status="Open" owner="Ravan" title="App crashes on open" />
+          <BugRow id={2} priority="P2" status="New" owner="Eddie" title="Misaligned border on panel" />
+        </tbody>
+      </table>
     )
   }
 });
